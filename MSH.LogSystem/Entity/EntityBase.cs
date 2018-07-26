d1edd4f29852783c77b114767ee5a3122957a56b
hai.liu <1093439315@qq.com>
2018-07-26 17:38:10 +0800

    添加Entity

---
+++ MSH.LogSystem/Entity/EntityBase.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class EntityBase
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        [BsonId]
+        public ObjectId Id { get; set; }
+    }
+}
---
+++ MSH.LogSystem/Entity/LogBase.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class LogBase : EntityBase
+    {
+        /// <summary>
+        /// 日志级别
+        /// </summary>
+        public short LogLevel { get; set; }
+        /// <summary>
+        /// 生成时间
+        /// </summary>
+        public DateTime CreationTime { get; set; }
+        /// <summary>
+        /// 日志内容
+        /// </summary>
+        public string Content { get; set; }
+        /// <summary>
+        /// 堆栈信息
+        /// </summary>
+        public string TraceInfo { get; set; }
+        /// <summary>
+        /// 业务Id
+        /// </summary>
+        public ObjectId BusinessId { get; set; }
+    }
+}
