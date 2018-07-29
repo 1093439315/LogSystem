84215ea5dc657f3f2c687e5bb49fe073282c77d2
雾月哥 <1093439315@qq.com>
2018-07-29 23:57:09 +0800

    登录页

--- logsystem-admin/src/router.js
+++ logsystem-admin/src/router.js
@@ -1,8 +1,19 @@
-const routers = [{
-    path: '/',
-    meta: {
-        title: ''
+const routers = [
+    //首页 默认页
+    {
+        path: '/',
+        meta: {
+            title: '首页'
+        },
+        component: (resolve) => require(['./views/index.vue'], resolve)
     },
-    component: (resolve) => require(['./views/index.vue'], resolve)
-}];
+    //登录页
+    {
+        path: '/login',
+        meta: {
+            title: '登录'
+        },
+        component: (resolve) => require(['./views/login/login.vue'], resolve)
+    }
+];
 export default routers;
---
+++ logsystem-admin/src/views/login/login.vue
@@ -0,0 +1,18 @@
+<style lang="less">
+    @import './login.less';
+</style>
+
+<template>
+    <div class="login" style="height: 100%">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'login'
+    };
+</script>
+
+<style scoped>
+
+</style>
